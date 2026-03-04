8a4f5d7c6dba610c8398ce18b787290b6fdc9259
pruek0704 <pruek0704@gmail.com>
2026-03-05 03:54:48 +0700

    add admin feature

---
+++ librarian/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('auth/', views.AdminLoginView.as_view(), name='admin_login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='admin_login'), name='logout'),
+    path('manage/', views.manage_books, name='manage_books'),
+    path('record/', views.active_borrows, name='active_borrows'),
+    path('borrow/', views.borrow_process, name='borrow_process'),
+    path('return/', views.return_book, name='return_book'),
+    path('users/', views.manage_users, name='manage_users'),
+    path('setting/', views.settings_view, name='settings_view'),
+]
